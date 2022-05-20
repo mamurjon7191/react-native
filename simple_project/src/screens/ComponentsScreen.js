38c6bb468b791fbdabdd08b4a3bd13e9d7c03412
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-05-20 22:16:39 +0500

    companenta yaratildi

---
+++ simple_project/src/screens/ComponentsScreen.js
@@ -0,0 +1,16 @@
+import react from "react";
+import { Text, StyleSheet } from "react-native";
+
+const ComponentsScreen = () => {
+  return (
+    <Text style={styles.textStyle}>This is componentScreen component</Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 40,
+  },
+});
+
+export default ComponentsScreen;
