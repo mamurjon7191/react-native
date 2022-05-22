b6a9cb3fa468333ace68139ae2272bc6627e2522
mamurjon7191 <ibragimovmamurjon19@gmail.com>
2022-05-22 09:20:59 +0500

    S

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, Button, TouchableOpacity, View } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>HomeScreen</Text>
+      <Button title="Go to components demo" />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,13 +1,36 @@
 import react from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, FlatList } from "react-native";
+
+// FlatList arreyni ichini aylanib chiqish uchun kerak boladi
 
 const ListScreen = () => {
-  return <Text style={styles.textStyle}>THIS IS LIST SCREEN COMPONENT</Text>;
+  const array = [
+    { friend: "Friend2 Age 22" },
+    { friend: "Friend3 Age 32" },
+    { friend: "Friend4 Age 21" },
+    { friend: "Friend1 Age 42" },
+    { friend: "Friend5 Age 52" },
+    { friend: "Friend6 Age 12" },
+    { friend: "Friend7 Age 62" },
+  ];
+  return (
+    <FlatList
+      // horizontal // yozilgan itemlarni vertikalmas garizantal qilib beradi
+      // showsHorizontalScrollIndicator={false} // garizantal qiganimizda pasda scrollni indikatori chiqib turadi oshani ochirish uchun shu yoziladi
+      keyExtractor={(mamur12) => mamur12.friend} // har biita elementga key berish uchun key extraktordan foydalamaiz
+      data={array}
+      renderItem={({ item }) => {
+        return <Text style={styles.textStyle}>{item.friend}</Text>;
+      }}
+    />
+  );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
     fontSize: 30,
+    marginHorizontal: 34, // bu column gap ga oxshab joy tashab beradi
+    marginVertical: 10, // bu row gap ga oxshab joy tashab beradi
   },
 });
 
